93efa2a88fcff681b7311a5037d1603e302c0058
wooqash <lukasz@zibeline.pl>
2023-12-30 01:51:52 +0100

    added basic internationlization functionality

---
+++ server/router/index.ts
@@ -0,0 +1,25 @@
+import "server-only";
+import { Router, schema } from "next-roots";
+
+export const router = new Router(schema);
+
+export function getLocales() {
+  return schema.locales;
+}
+
+export function getPageLocale() {
+  return router.getLocaleFromHref(Router.getPageHref());
+}
+
+// export function getAboutHref(locale: string = getPageLocale()) {
+//   return router.getHref("/kontakt", { locale });
+// }
+
+export function getContactsHref(locale: string = getPageLocale()) {
+  console.log(router.getHref("/kontakt", { locale }), locale)
+  return router.getHref("/kontakt", { locale });
+}
+
+export function getHomeHref(locale: string = getPageLocale()) {
+  return router.getHref("/", { locale });
+}
